a88b45ddecca40a6857e5f73c9b9cba631bd0d35
Steve987321 <Stevebudding123@gmail.com>
2023-02-15 21:36:48 +0100

    changed players hand gripping

--- Assets/Scripts/Player/PlayerHandGrips.cs
+++ Assets/Scripts/Player/PlayerHandGrips.cs
@@ -2,11 +2,17 @@ using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerHands : MonoBehaviour
+public class PlayerHandGrips : MonoBehaviour
 {
+    [SerializeField] private PlayerGunHandler _gunHandler;
+    public bool IsDominantHand = false;
+
     [HideInInspector] public GameObject HeldObject;
-    
-    private bool _mdown = false;
+
+    // whether gripping key is pressed
+    private bool _grippingBtnDown = false;
+
+    // wheter player is holding an object
     private bool _isHolding = false;
 
     private Rigidbody _rb;
@@ -16,13 +22,23 @@ public class PlayerHands : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        _mdown = Input.GetKey(KeyCode.Mouse1);
-        
-        if (_isHolding) return;
-        if (!_mdown) return;
+        _grippingBtnDown = Input.GetKey(KeyCode.Mouse1);
 
+        if (!_grippingBtnDown && _isHolding)
+        {
+            Debug.Log("released " + HeldObject.name);
+            if (HeldObject.GetComponent<FixedJoint>() != null)
+                Destroy(HeldObject.GetComponent<FixedJoint>());
+            _isHolding = false;
+        }
+
+        if (IsDominantHand && _gunHandler.HasWeapon) return;
+        if (_isHolding) return;
+        if (!_grippingBtnDown) return;
+
+        // look for object to grab with prop tag
         var list = GameObject.FindGameObjectsWithTag("Prop").Select(item => item.transform);
         var transforms = list as Transform[] ?? list.ToArray();
         if (transforms.Length <= 0) return;
@@ -64,15 +80,4 @@ public class PlayerHands : MonoBehaviour
         //}
     }
 
-
-    void FixedUpdate()
-    {
-        if (!_mdown && _isHolding)
-        {
-            Debug.Log("released " + HeldObject.name);
-            if (HeldObject.GetComponent<FixedJoint>() != null)
-                Destroy(HeldObject.GetComponent<FixedJoint>());
-            _isHolding = false;
-        }
-    }
 }
