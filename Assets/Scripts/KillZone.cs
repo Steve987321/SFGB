fbdc0213dbfabc43522488da3064c68bb80f92b3
Steve987321 <Stevebudding123@gmail.com>
2023-03-25 14:02:17 +0100

    renamed PlayerResetZone to KillZone

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -2,10 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// will damage or destroy things that are entering the collision area
-/// </summary>
-public class KillZone : MonoBehaviour
+public class PlayerResetZone : MonoBehaviour
 {
     public float Damage = 10f;
 
